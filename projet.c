16f46084d98c688a5d0d35b816a9858e175320e8
Chaimaa ET-Toumy <81262223+chaima-proge@users.noreply.github.com>
2021-11-23 16:18:53 +0100

    projet

--- projet.c
+++ projet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <string.h> 
 struct Comptes_Bancaires {
 	char cin[20];
@@ -8,18 +7,20 @@ struct Comptes_Bancaires {
 	char prenom[50];
 	float montant;
 };
-int max = 10000;
-//size total
-int recherche_cin(struct Comptes_Bancaires bank[10000], int size, char *rech)
-{
+int max = 1000;
+struct Comptes_Bancaires compte[1000];
+int    recherche_cin(){
+    char cin[20];
     int i;
-    int index = -1;
-
-    for (i=0; i<size; i++) {
-        if (strcmp(rech, bank[i].cin) == 0)
-            index = i;
+    printf("donner cin : ");
+    scanf("%s",cin);
+    for(i = 0;i<sizeof(max);i++)
+    {
+        if(strstr(cin,compte[i].cin))
+        {
+            return i;
+        }
     }
-    return index;
 }
 
 int i;
@@ -38,7 +39,7 @@ void menu_principal()
 
 int main() {
 	menu_principal();
-	struct Comptes_Bancaires compte[max];
+	//struct Comptes_Bancaires compte[max];
 	int n,i=0,ind_cin,total=0;
 	int choix,choix3;
 	char rech_cin[20];
@@ -51,8 +52,8 @@ int main() {
 	{
 		switch (n) {
 			case 1 : // cin //nom et prenom // montant
-			    	        system("cls");
-			         	printf("\n\t\t\t****************** Creer le Compte ******************\n");
+			    	system("cls");
+			        printf("\n\t\t\t****************** Creer le Compte ******************\n");
 					printf("   Entrez le cin du client : ");
 					scanf("%s",compte[i].cin);
 					printf("   Entrez le nom du client  : ");
@@ -63,21 +64,21 @@ int main() {
 					scanf("%f",&compte[i].montant);
 					printf("   le compte a ete cree avec succes\n\n");
 								       
-
-					 i++;
-                                         if (i > max - 1) {
-                                         i = 0;
-                                         printf("le stockage est saturé les anciennes informations seront supprimées!");
-                                         }
-                                         total++; // 1000
-			               	if (total > max)
+					i++;
+                    if (i > max - 1) 
+					{
+					i = 0;
+                    printf("le stockage est saturé les anciennes informations seront supprimées!");
+                    }
+                    total++; // 1000
+			        if (total > max)
 					total = max; // ex max = 999
                   
 				    break;
 			case 2 :
-				        system("cls");
-				        printf("\n\t\t\t****************** Creer le Compte ******************\n");
-                                        printf("   Entrez le cin du client : ");
+				    system("cls");
+				    printf("\n\t\t\t****************** Creer le Compte ******************\n");
+                    printf("   Entrez le cin du client : ");
 					scanf("%s",compte[i].cin);
 					printf("   Entrez le nom du client  : ");
 					scanf("%s",compte[i].nom);
@@ -86,71 +87,57 @@ int main() {
 					printf("   Entrer le montant du client: ");
 					scanf("%f",&compte[i].montant);
 					printf("   le compte a ete cree avec succes\n\n");
-                                        i++;
-                                        if (i > max - 1) {
-                    		        i = 0;
-                                        printf("\t le stockage est saturé les anciennes informations seront supprimées!");
-                                        }
-                                        total++; 
-				        if (total > max)
+                    i++;
+                    if (i > max - 1) {
+                   	i = 0;
+                    printf("\t le stockage est saturé les anciennes informations seront supprimées!");
+                                         }
+                    total++; 
+				    if (total > max)
 					total = max; 
-					printf("entrer 1 pour saisir autre personne ou 2 pour retourner au menu principale : ");
-                                       scanf("%d", &choix);
-				       while (choix != 2)
-                                       {
-                                       if (choix == 1) {
-						printf("   Entrez le cin du client : ");
-						scanf("%s",compte[i].cin);
-						printf("   Entrez le nom du client  : ");
-					        scanf("%s",compte[i].nom);
-					        printf("   Entrez le prenom du client  : ");
-					        scanf("%s",compte[i].prenom);
-					        printf("   Entrer le montant du client: ");
-					        scanf("%f",&compte[i].montant);
-					        i++;
-					        if (i > max - 1)
-					        {
-                           		        i = 0;
-                           		        printf("le stockage est saturé les anciennes informations seront supprimées!");
-                   		 	        }
-                   		 	        total++; 
-				                if (total > max)
+				    printf("entrer 1 pour saisir autre personne ou 2 pour retourner au menu principale : ");
+                    scanf("%d", &choix);
+		            while (choix != 2)
+					{
+                    if (choix == 1) {
+					printf("   Entrez le cin du client : ");
+					scanf("%s",compte[i].cin);
+					printf("   Entrez le nom du client  : ");
+				    scanf("%s",compte[i].nom);
+     		        printf("   Entrez le prenom du client  : ");
+				    scanf("%s",compte[i].prenom);
+					printf("   Entrer le montant du client: ");
+				    scanf("%f",&compte[i].montant);
+				    i++;
+					if (i > max - 1)
+				    {
+                     i = 0;
+                     printf("le stockage est saturé les anciennes informations seront supprimées!");
+                     }
+                   	 total++; 
+				      if (total > max)
 					         total = max; 
-				    	         }
-				        else {
+				    }
+				     else {
                                                   printf("L'option n'existe pas \n");
-				             }
-                                        printf("entrer 1 pour saisir autre personne ou 2 pour retourner au menu principale : ");
-                                	scanf("%d", &choix);
-                                       }
+				            }
+                    printf("entrer 1 pour saisir autre personne ou 2 pour retourner au menu principale : ");
+                  	scanf("%d", &choix);
+                   }
 					break;
 			case 3 :
-                                 system("cls");
+                                system("cls");
                                 printf("les operations : ");
-				printf("Entrer 1 pour Retrait ou 2 pour Depot ou 3 pour annuler :");
-				scanf("%d", &choix3);
-
-				// Retrait
+			            	    printf("Entrer 1 pour Retrait ou 2 pour Depot ou 3 pour annuler : ");
+			  					scanf("%d", &choix3);
 			    	if (choix3 == 1)
-			       	{
-						printf("Entrer  le cin : ");
-						scanf("%s", rech_cin);
-
+			       	{	
 						// idice de compte
-						ind_cin = recherche_cin(compte, total, rech_cin);
-	
-						//  non trouve
-							if (ind_cin == -1)
-								{
-									printf("Le cin n'existe pas \n");
-								}
-
-							// cin trouve
-							else
-							{
+						ind_cin = recherche_cin();
+	                    printf("\n%s %s %s %f\n",compte[ind_cin].cin,compte[ind_cin].nom,compte[ind_cin].prenom,compte[ind_cin].montant);
 								// montant a retire
-								printf("Entrer le montant :");
-								scanf("%f", &montantEntree);
+					    printf("Entrer le montant :");
+					    scanf("%f", &montantEntree);
 
 										if (montantEntree > compte[ind_cin].montant)
 										printf("Montant invalide!\n");
@@ -159,30 +146,23 @@ int main() {
 										compte[ind_cin].montant -= montantEntree;
 										printf("Termine avec succes \n");
 										}
-							}
+	                    printf("\n%s %s %s %f\n",compte[ind_cin].cin,compte[ind_cin].nom,compte[ind_cin].prenom,compte[ind_cin].montant);
+
 			    	}
 			  		else if (choix3 == 2)
 				   {
-						printf("Entrer le cin : ");
-						scanf("%s",rech_cin);
-						ind_cin = recherche_cin(compte, total, rech_cin);
-
-							if (ind_cin == -1)
-								{
-									printf("Le cin n'existe pas \n");
-								}
-
-							// cin trouve
-							else
-							{
-								// montant a retire
-								printf("Entrer le montant :");
+				        ind_cin = recherche_cin();
+				        printf("\n%s %s %s %f\n",compte[ind_cin].cin,compte[ind_cin].nom,compte[ind_cin].prenom,compte[ind_cin].montant);
+			
+	         					printf("Entrer le montant :");
 								scanf("%f", &montantEntree);
 								compte[ind_cin].montant += montantEntree;
 								printf("Termine avec succes \n");
+								printf("\n%s %s %s %f\n",compte[ind_cin].cin,compte[ind_cin].nom,compte[ind_cin].prenom,compte[ind_cin].montant);
 										
-							}
+					
 					}
+					break;
 			default :
 					printf("Operation n'existe pas  \n\n");
 					break;
